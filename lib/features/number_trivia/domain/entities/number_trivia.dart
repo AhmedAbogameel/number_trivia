fa835de6ebd5bf3a8434b8bd05da96507fbe61ab
AhmedAbogameel <gamilco81@gmail.com>
2022-02-07 20:34:48 +0200

    Initial commit

---
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class NumberTrivia extends Equatable {
+
+  final String text;
+  final int number;
+
+  NumberTrivia({required this.text, required this.number});
+
+  @override
+  List<Object?> get props => [this.text, this.number];
+
+}
---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+
+import 'get_concrete_number_trivia_test.mocks.dart';
+
+@GenerateMocks([NumberTriviaRepository])
+void main() {
+  late GetConcreteNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumber = 1;
+  final tNumberTrivia = NumberTrivia(text: "test", number: 1);
+
+  test('should get trivia for the number from the repository', () async {
+    // arrange
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber))
+        .thenAnswer((_) async => Right(tNumberTrivia));
+    // act
+    final result = await usecase.execute(number: tNumber);
+    // assert
+    expect(result, Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+  });
+}
