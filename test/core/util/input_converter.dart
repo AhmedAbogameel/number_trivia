45a55ba5e45e4fbffd0e7357b97901d87a436f77
AhmedAbogameel <gamilco81@gmail.com>
2022-02-14 18:34:46 +0200

    - Bloc Scaffolding & Input Conversion

---
+++ test/core/util/input_converter.dart
@@ -0,0 +1,50 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test(
+      'should return an integer when the string represents an unsigned integer',
+      () async {
+        // arrange
+        final str = '123';
+        // act
+        final result = inputConverter.stringToUnsignedInteger(str);
+        // assert
+        expect(result, Right(123));
+      },
+    );
+
+    test(
+      'should throw a Failure when the string is not integer',
+          () async {
+        // arrange
+        final str = 'abc';
+        // act
+        final result = inputConverter.stringToUnsignedInteger(str);
+        // assert
+        expect(result, Left(InvalidInputFailure()));
+      },
+    );
+
+    test(
+      'should throw a Failure when the string is negative integer',
+          () async {
+        // arrange
+        final str = '-123';
+        // act
+        final result = inputConverter.stringToUnsignedInteger(str);
+        // assert
+        expect(result, Left(InvalidInputFailure()));
+      },
+    );
+  });
+
+}
